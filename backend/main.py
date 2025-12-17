0cc005c02bae885dd528b16d40e15018f907b42d
Sheeksha Joyseeree <sheekshajoyseeree@gmail.com>
2025-12-16 22:59:22 -0500

    added Netlify link

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,9 @@ app = FastAPI(title="S1000D DM Analyzer")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", 
+                   "http://127.0.0.1:5173",
+                   "https://s1000d-dm-analyzer.netlify.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
