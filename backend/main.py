3c290c706ba1a53a739e75c3bdaaf3bb768fabe6
Sheeksha Joyseeree <sheekshajoyseeree@gmail.com>
2025-12-16 23:09:13 -0500

    updated cors

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173", 
                    "http://127.0.0.1:5173",
-                   "https://s1000d-dm-analyzer.netlify.app/"],
+                   "https://s1000d-dm-analyzer.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
